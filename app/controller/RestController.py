91abf8f39416db89ae774a57ef22cef182262875
trevarmand <trevor.armand13@verizon.net>
2020-02-09 19:02:36 -0500

    Started wiring up regression code. First endpoint is complete. DAO / connection is incomplete

--- app/controller/RestController.py
+++ app/controller/RestController.py
@@ -1,1 +1,20 @@
 # Exposes RESTful endpoints for calling on model functions.
+from flask import Flask
+from flask import request
+from app.model import BatterModel
+
+app = Flask(__name__)
+
+
+@app.route("/status")
+def check_status():
+    return "online"
+
+# Given a batter name in the format first+last, and a pitcher name first+last, returns the probability a batter hits
+@app.route('/hit', methods=['GET'])
+def get_hit_probability():
+    batter = request.args.get("batter")
+    pitcher = request.args.get("pitcher")
+    result = BatterModel.determine_hit_likelihood(batter, pitcher)
+    print(result)
+    return '{probability:' + str(123) + "}"
--- app/model/BatterModel.py
+++ app/model/BatterModel.py
@@ -1,1 +1,35 @@
 # A model for predicting batter performance based on pitch physics.
+
+from sklearn import linear_model
+import pandas as pd
+from app.model.datastore import MySqlDao
+
+
+class RegressionResult:
+
+    def __init__(self, probability, rsquare):
+        self.probabiliy = probability
+        self.rsquare = rsquare
+
+
+# Returns a linear model for this batter based on his history against all pitches ever
+def get_batter_model(batter_name):
+    df = MySqlDao.get_batter_history(batter_name)
+    target = pd.DataFrame(data=df, columns=["hit"])
+    return linear_model.LinearRegression().fit(X=df, y=target["hit"])
+
+
+# Returns a data frame
+def get_pitcher_averages(batter_name, pitcher_name):
+    return MySqlDao.get_pitcher_averages(batter_name, pitcher_name)
+
+
+# Returns a number indicating the hit likelihood and the r^2
+def determine_hit_likelihood(batter_name, pitcher_name):
+    batter_model = get_batter_model(batter_name)
+    pitcher_averages = get_pitcher_averages(batter_name, pitcher_name)
+    # pitcher avg may need to be converted to an array
+    result = batter_name.predict(pitcher_averages)
+    return RegressionResult(result, batter_model.score)
+
+
